7d27ff824883d7535de38158d58fe4df9c7c8887
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-08-11 16:46:01 -0300

    improvement: adiciona status 500 nos endpoints /product , /product/:id

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,19 +1,32 @@
 const productsService = require('../services/productsService');
 
+const ERROR_MESSAGE = 'Server error';
+
 const getAll = async (req, res) => { 
+  try {
     const products = await productsService.getAll();
     return res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(ERROR_MESSAGE);
+  }
 };
 
 const getById = async (req, res) => { 
   const { id } = req.params;
-  const product = await productsService.getById(id);
-  
-  if (!product) {
-    return res.status(404).json({ message: 'Product not found' });
+
+  try {
+    const product = await productsService.getById(id);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    return res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(ERROR_MESSAGE);
   }
-   
-  return res.status(200).json(product);
 };
 
 module.exports = {
